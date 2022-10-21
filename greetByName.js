4534a9b685f443b7fd1eb2f0117e717bb7a5d8ae
Charonn <charonn.edwards412@gmail.com>
2022-10-21 00:40:05 -0400

    added ageCalculator,greetByName.html, greetByName.js

---
+++ greetByName.js
@@ -0,0 +1,10 @@
+let hiBtn = document.getElementById('hiBtn');
+hiBtn.addEventListener('click',greetUser);
+
+function greetUser() {
+    console.log('hiBtn was clicked')
+    let yourName = document.querySelector('output')
+    
+}
+
+
